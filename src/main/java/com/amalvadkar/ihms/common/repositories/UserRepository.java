b2f82879a1d2698ccd80ac9ca70f837c9a4e41f5
Abhishek Malvadkar <abhi@dev.com>
2024-05-18 11:46:22 +0530

    GH-1 : Added AppException and changes User repo

--- src/main/java/com/amalvadkar/ihms/common/repositories/UserRepository.java
+++ src/main/java/com/amalvadkar/ihms/common/repositories/UserRepository.java
@@ -4,4 +4,7 @@ import com.amalvadkar.ihms.common.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    UserEntity findByEmailAndDeleteFlagIsFalseAndActiveIsTrue(String email);
+
 }
